f1b1beddb17236f0e1bfb1232be87706f78c5414
zhihao lin <cl121@illinois.edu>
2023-04-24 12:20:33 -0500

    [0424] create data split

--- split.py
+++ split.py
@@ -6,7 +6,7 @@ dir_calib = 'calib_2'
 dir_image = 'image_2'
 dir_label = 'label_2'
 dir_velodyne = '/hdd/datasets/KITTI-360/data_3d_raw/2013_05_28_drive_0000_sync/velodyne_points/data'
-dir_output = 'kitti360'
+dir_output = '/hdd/mmdetection3d/data/kitti360_2'
 os.makedirs(dir_output, exist_ok=True)
 
 def rename():
@@ -42,74 +42,106 @@ def create_split():
     # train: 25%, validation: 25%, test: 50% -> follow the distribution of kitti dataset
     dir_imgsets = os.path.join(dir_output, 'ImageSets')
     os.makedirs(dir_imgsets, exist_ok=True)
+    dir_imgids = os.path.join(dir_output, 'ImageID')
+    os.makedirs(dir_imgids, exist_ok=True)
+
     cut = int(data_num * 0.5)
-    split_train_valid = img_ids[:cut]
-    split_test = img_ids[cut:]
-    sample = (np.arange(len(split_train_valid)) % 2) == 0
-    split_train = split_train_valid[sample]
-    split_valid = split_train_valid[~sample]
+    ids_train_valid = img_ids[:cut]
+    ids_test = img_ids[cut:]
+    sets_train_valid = np.arange(len(ids_train_valid))
+    sets_test = np.arange(len(ids_test))
+
+    sample = (sets_train_valid % 2) == 0
+    ids_train = ids_train_valid[sample]
+    ids_valid = ids_train_valid[~sample]
+    sets_train = sets_train_valid[sample]
+    sets_valid = sets_train_valid[~sample]
 
     with open(os.path.join(dir_imgsets, 'train.txt'), 'w') as file:
-        for i in range(len(split_train)):
-            line = '{:0>6d}\n'.format(split_train[i])
+        for i in range(len(sets_train)):
+            line = '{:0>6d}\n'.format(sets_train[i])
+            file.write(line)
+    
+    with open(os.path.join(dir_imgids, 'train.txt'), 'w') as file:
+        for i in range(len(ids_train)):
+            line = '{:0>6d}\n'.format(ids_train[i])
             file.write(line)
 
     with open(os.path.join(dir_imgsets, 'val.txt'), 'w') as file:
-        for i in range(len(split_valid)):
-            line = '{:0>6d}\n'.format(split_valid[i])
+        for i in range(len(sets_valid)):
+            line = '{:0>6d}\n'.format(sets_valid[i])
+            file.write(line)
+    
+    with open(os.path.join(dir_imgids, 'val.txt'), 'w') as file:
+        for i in range(len(ids_valid)):
+            line = '{:0>6d}\n'.format(ids_valid[i])
             file.write(line)
 
     with open(os.path.join(dir_imgsets, 'trainval.txt'), 'w') as file:
-        for i in range(len(split_train_valid)):
-            line = '{:0>6d}\n'.format(split_train_valid[i])
+        for i in range(len(sets_train_valid)):
+            line = '{:0>6d}\n'.format(sets_train_valid[i])
+            file.write(line)
+
+    with open(os.path.join(dir_imgids, 'trainval.txt'), 'w') as file:
+        for i in range(len(ids_train_valid)):
+            line = '{:0>6d}\n'.format(ids_train_valid[i])
             file.write(line)
 
     with open(os.path.join(dir_imgsets, 'test.txt'), 'w') as file:
-        for i in range(len(split_test)):
-            line = '{}\n'.format(split_test[i])
+        for i in range(len(sets_test)):
+            line = '{:0>6d}\n'.format(sets_test[i])
+            file.write(line)
+    
+    with open(os.path.join(dir_imgids, 'test.txt'), 'w') as file:
+        for i in range(len(ids_test)):
+            line = '{:0>6d}\n'.format(ids_test[i])
             file.write(line)
 
-def copy_data_with_ids(dir_input, dir_output, ids):
-    def contain(i, array):
-        a = array - i
-        zero_num = np.sum(a == 0)
-        if zero_num > 0:
-            return True
+def copy_data_with_ids(dir_input, dir_output, sets, ids):
+    def match_set_with_id(i, sets, ids):
+        match = (ids - i) == 0
+        if np.sum(match) == 0:
+            return -1
         else:
-            return False
+            return sets[match].item()
     
     os.makedirs(dir_output, exist_ok=True)
     paths_input = sorted([os.path.join(dir_input, name) for name in os.listdir(dir_input)])
     for path in tqdm(paths_input):
         path_id = get_img_id(path, int_id=True)
-        if contain(path_id, ids):
-            path_output = os.path.join(dir_output, '{:0>6d}{}'.format(path_id, path[-4:]))
+        s = match_set_with_id(path_id, sets, ids)
+        if s >= 0:
+            path_output = os.path.join(dir_output, '{:0>6d}{}'.format(s, path[-4:]))
             command = 'cp {} {}'.format(path, path_output)
             os.system(command)
 
 def split_data():
-    split_trainval_txt = os.path.join(dir_output, 'ImageSets', 'trainval.txt')
-    split_test_txt  = os.path.join(dir_output, 'ImageSets', 'test.txt')
+    sets_trainval_txt = os.path.join(dir_output, 'ImageSets', 'trainval.txt')
+    sets_test_txt  = os.path.join(dir_output, 'ImageSets', 'test.txt')
+    ids_trainval_txt = os.path.join(dir_output, 'ImageID', 'trainval.txt')
+    ids_test_txt  = os.path.join(dir_output, 'ImageID', 'test.txt')
 
-    split_trainval = np.genfromtxt(split_trainval_txt).astype(np.int32)
-    split_test  = np.genfromtxt(split_test_txt).astype(np.int32)
+    sets_trainval = np.genfromtxt(sets_trainval_txt).astype(np.int32)
+    sets_test  = np.genfromtxt(sets_test_txt).astype(np.int32)
+    ids_trainval = np.genfromtxt(ids_trainval_txt).astype(np.int32)
+    ids_test  = np.genfromtxt(ids_test_txt).astype(np.int32)
     
     # calib
-    copy_data_with_ids(dir_calib, os.path.join(dir_output, 'training', 'calib'), split_trainval)
-    copy_data_with_ids(dir_calib, os.path.join(dir_output, 'testing', 'calib'), split_test)
+    copy_data_with_ids(dir_calib, os.path.join(dir_output, 'training', 'calib'), sets_trainval, ids_trainval)
+    copy_data_with_ids(dir_calib, os.path.join(dir_output, 'testing', 'calib'), sets_test, ids_test)
 
     # label
-    copy_data_with_ids(dir_label, os.path.join(dir_output, 'training', 'label_2'), split_trainval)
-    copy_data_with_ids(dir_label, os.path.join(dir_output, 'testing', 'label_2'), split_test)
+    copy_data_with_ids(dir_label, os.path.join(dir_output, 'training', 'label_2'), sets_trainval, ids_trainval)
+    copy_data_with_ids(dir_label, os.path.join(dir_output, 'testing', 'label_2'), sets_test, ids_test)
     
     # image
-    copy_data_with_ids(dir_image, os.path.join(dir_output, 'training', 'image_2'), split_trainval)
-    copy_data_with_ids(dir_image, os.path.join(dir_output, 'testing', 'image_2'), split_test)
+    copy_data_with_ids(dir_image, os.path.join(dir_output, 'training', 'image_2'), sets_trainval, ids_trainval)
+    copy_data_with_ids(dir_image, os.path.join(dir_output, 'testing', 'image_2'), sets_test, ids_test)
 
     # velodyne
-    copy_data_with_ids(dir_velodyne, os.path.join(dir_output, 'training', 'velodyne'), split_trainval)
-    copy_data_with_ids(dir_velodyne, os.path.join(dir_output, 'testing', 'velodyne'), split_test)
+    copy_data_with_ids(dir_velodyne, os.path.join(dir_output, 'training', 'velodyne'), sets_trainval, ids_trainval)
+    copy_data_with_ids(dir_velodyne, os.path.join(dir_output, 'testing', 'velodyne'), sets_test, ids_test)
 
 if __name__ == '__main__':
-    # create_split()
+    create_split()
     split_data()
